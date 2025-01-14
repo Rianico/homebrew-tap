e82e184833ac32726197f6552df775017e059f0e
zkx <zhxuankun@163.com>
2025-01-15 00:27:21 +0800

    [Release] MemToy 0.2.1

--- Formula/MemToy.rb
+++ Formula/MemToy.rb
@@ -2,7 +2,7 @@ class Memtoy < Formula
   desc "Rianico's memorization toy tool.'"
   homepage "https://github.com/Rianico/MemToy"
   url "https://github.com/Rianico/MemToy/releases/download/0.2.1/MemToy.dmg"
-  sha256 "4582a64d590016beed5872ca2d809a743911e8b6fb529d1dea8babdbe81925d9"
+  sha256 "fee60b43f86f90c88aa416106615613c6cdad04b6930646599fbb0ecfe5d3626"
   version "0.2.1"
 
   def install
