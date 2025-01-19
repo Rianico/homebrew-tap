3a204098733d0fd954edf63b030939054869598d
zkx <zhxuankun@163.com>
2025-01-20 00:35:24 +0800

    [release] MemToy 0.2.3

--- Casks/memtoy.rb
+++ Casks/memtoy.rb
@@ -1,6 +1,6 @@
 cask "memtoy" do
-  version "0.2.2"
-  sha256 "b4423a683ed64e7643a70243b76c944cf841c5d86e0f5c57b82834ad0612a77b"
+  version "0.2.3"
+  sha256 "75119882176f465a23007d2b0ee13392a7c8709c5a162d175db8bfd61cf879c5"
 
   url "https://github.com/Rianico/MemToy/releases/download/#{version}/MemToy.dmg"
   name "MemToy"
