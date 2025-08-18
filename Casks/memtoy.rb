d56a944de5473b7b049697903b0ff80111a3c8d2
zkx <zhxuankun@163.com>
2025-08-18 16:16:03 +0800

    update(memtoy): update to version 1.1.0

--- Casks/memtoy.rb
+++ Casks/memtoy.rb
@@ -1,8 +1,8 @@
 cask "memtoy" do
-  version "1.0.2"
-  sha256 "1e36d8ad447d1522e467d5fa0764c31bccadfcbf61244c29c1153b675610dda6"
+  version "1.1.0"
+  sha256 "996b52d8c7d15d2f8dbd05bc2eeccd29d7d9e781c39bf5239af95f6b9ccbed54"
 
-  url "https://github.com/Rianico/MemToy/releases/download/#{version}/MemToy.dmg"
+  url "https://github.com/Rianico/MemToy/releases/download/#{version}/MemToy_#{version}_aarch64.dmg"
   name "MemToy"
   desc "Rianico's memorization toy tool"
   homepage "https://github.com/Rianico/MemToy"
